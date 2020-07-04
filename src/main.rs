6c479b1b763231fd0951475d414f89bd2da4e173
daiju81 <1d07c.m8k.prv@gmail.com>
2020-07-04 19:18:24 +0900

    数あて

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use std::io;
+extern crate rand;
 
+use std::io;
+use rand::Rng;
 fn main() {
   println!("Guess the number!");
+  let secret_number = rand::thread_rng().gen_range(1, 101);
+  println!("The secret number is: {}", secret_number); //秘 密 の 数 字 は 次 の 通り: {}
+  
   println!("Please input your guess.");
-
+  
   let mut guess = String::new();
-  io::stdin().read_line(&mut guess).expect("Failed to read line"); 
-
-  println!("you guessd: {}", guess);
+  io::stdin().read_line( & mut guess)
+    .expect("Failed to read line");
+  println!("You guessed: {}", guess);
 }
