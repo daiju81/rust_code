93d959f944a9f2c272d6a0491622c9ca9ac51231
daiju81 <1d07c.m8k.prv@gmail.com>
2020-07-04 19:31:18 +0900

    数あてゲーム

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 fn main() {
   println!("Guess the number!");
@@ -10,7 +11,19 @@ fn main() {
   println!("Please input your guess.");
   
   let mut guess = String::new();
-  io::stdin().read_line( & mut guess)
-    .expect("Failed to read line");
+  
+  io::stdin().read_line(&mut guess)
+  .expect("Failed to read line");
+  
+  let guess: u32 = guess.trim().parse()
+  .expect("Please type a number!"); //数 値 を 入 力 し て く だ さい！
+
   println!("You guessed: {}", guess);
+
+  
+  match guess.cmp(&secret_number) {
+  Ordering::Less => println!("Too small!"), //小 さ す ぎ！
+  Ordering::Greater => println!("Too big!"), //大 き す ぎ！
+  Ordering::Equal => println!("You win!"), // や っ た ね！
+  }
 }
